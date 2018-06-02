75fec755ab66975d403892592d2655d4ec85d2f5
Eduardo Zanela <eduardozanela@Plutao.local>
2018-06-02 17:20:44 -0300

    More endpoints

--- src/main/java/br/com/batepapo/batepapouserssoapwebservices/BatePapoUsersSoapWebServiceApplication.java
+++ src/main/java/br/com/batepapo/batepapouserssoapwebservices/BatePapoUsersSoapWebServiceApplication.java
@@ -6,12 +6,7 @@ import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class BatePapoUsersSoapWebServiceApplication extends SpringBootServletInitializer {
-	
-	@Override
-	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-		return application.sources(BatePapoUsersSoapWebServiceApplication.class);
-	}
+public class BatePapoUsersSoapWebServiceApplication {
 	
 	public static void main(String[] args) {
 		SpringApplication.run(BatePapoUsersSoapWebServiceApplication.class, args);
